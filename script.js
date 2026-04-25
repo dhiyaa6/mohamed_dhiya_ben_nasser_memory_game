913ca2bb658ce3af669886f88e97d16279055cd9
Dhiya <dhiya1234.live.fr4@gmail.com>
2026-04-25 19:38:14 +0100

    Enhance game with score, moves, flip animation and images instead of emojis

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
 const board = document.getElementById("game-board");
-let cardsArray = ["🍎","🍌","🍇","🍓","🍒","🍍","🥝","🍉"];
-cardsArray = [...cardsArray, ...cardsArray]; 
+
+
+let cardsArray = [
+  "img1.png","img2.png","img3.png","img4.png",
+  "img5.png","img6.png","img7.png","img8.png"
+];
+
+cardsArray = [...cardsArray, ...cardsArray];
 cardsArray.sort(() => 0.5 - Math.random());
+
 let firstCard = null;
 let secondCard = null;
 let lockBoard = false;
-cardsArray.forEach((emoji) => {
+
+let score = 0;
+let moves = 0;
+
+cardsArray.forEach((img) => {
   const card = document.createElement("div");
   card.classList.add("card");
-  card.dataset.value = emoji;
-  card.innerText = "";
+
+  card.innerHTML = `
+    <div class="card-inner">
+      <div class="card-front"></div>
+      <div class="card-back">
+        <img src="images/${img}" />
+      </div>
+    </div>
+  `;
+
+  card.dataset.value = img;
+
   card.addEventListener("click", flipCard);
+
   board.appendChild(card);
 });
+
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
 
-  this.innerText = this.dataset.value;
-  this.classList.add("flipped");
+  this.classList.add("flip");
 
   if (!firstCard) {
     firstCard = this;
@@ -30,21 +52,24 @@ function flipCard() {
 }
 
 function checkMatch() {
+  moves++;
+  document.getElementById("moves").innerText = moves;
+
   let isMatch = firstCard.dataset.value === secondCard.dataset.value;
 
   if (isMatch) {
+    score++;
+    document.getElementById("score").innerText = score;
     resetCards();
   } else {
     lockBoard = true;
-    setTimeout(() => {
-      firstCard.innerText = "";
-      secondCard.innerText = "";
 
-      firstCard.classList.remove("flipped");
-      secondCard.classList.remove("flipped");
+    setTimeout(() => {
+      firstCard.classList.remove("flip");
+      secondCard.classList.remove("flip");
 
       resetCards();
-    }, 800);
+    }, 1000);
   }
 }
 
